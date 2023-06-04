c454336b743947f9a8aefa8bb874757dce7d1113
AyimWilliam <odhisayim@gmail.com>
2023-06-04 18:52:17 +0300

    Completed assignment

---
+++ lib/migrations/versions/cd0bc0829e36_add_user_model.py
@@ -0,0 +1,24 @@
+"""Add User model
+
+Revision ID: cd0bc0829e36
+Revises: a28af33e6866
+Create Date: 2023-06-04 18:12:49.396116
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'cd0bc0829e36'
+down_revision = 'a28af33e6866'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
